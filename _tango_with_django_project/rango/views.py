f879bbfb451f0985ebd6adfc21917c79e2afa58a
Tomasz Wojciechowski <wojciechowski@hushmail.com>
2015-08-05 17:21:42 +0200

    chapter 5 completed

--- _tango_with_django_project/rango/views.py
+++ _tango_with_django_project/rango/views.py
@@ -13,4 +13,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango'>Index</a>")
+    context_dict = {'about_boldmessage': "Bold message dla About"}
+    return render(request, 'rango/about.html', context_dict)
