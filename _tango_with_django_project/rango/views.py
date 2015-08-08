5bd56fcb7ce55b45c740e4713558f465361f9c24
Tomasz Wojciechowski <wojciechowski@hushmail.com>
2015-08-08 20:23:16 +0200

    fixed displaying both pictures of Rango

--- _tango_with_django_project/rango/views.py
+++ _tango_with_django_project/rango/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the clint.
-    # We make use of the shortcut function to make out lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     return render(request, 'rango/index.html', context_dict)
 
